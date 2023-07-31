360b1fe169c0273973dc977ac43b4c581f4a3f2c
ammar <ammarstar0@gmail.com>
2023-07-31 23:38:03 +0700

    pertama

---
+++ script.js
@@ -0,0 +1,7 @@
+function fullview(imgLink) {
+  document.querySelector('.id').src=imgLink;
+  document.querySelector('.full-image').style.display='block';
+}
+function tutups() {
+  document.querySelector('.full-image').style.display='none';
+}
